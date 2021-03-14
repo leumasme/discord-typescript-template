6052ea21a7e672e45ea784824ea5f146cba8df0b
Temm <samumert@gmail.com>
2021-03-14 19:08:38 +0100

    Create the basic discord bot login

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-var hello: string = "Hello, World!";
-console.log(hello);
+import { Client } from "discord.js"
+import { config as loadEnv } from "dotenv"
+// loadEnv()
+
+var bot = new Client();
+
+bot.on("ready", ()=>{
+    console.log("Hello, Discord!")
+})
+
+bot.login(process.env.TOKEN).catch((e: Error)=>{
+    console.log(e.message);
+    console.log("You should have a Discord Token in your .env File")
+})
