9f493e37a034cd5563295a324ab0af6b8e289b10
Temm <samumert@gmail.com>
2021-03-15 00:01:52 +0100

    Add command permissions

--- src/command_loader.ts
+++ src/command_loader.ts
@@ -1,4 +1,4 @@
-import { Client, Message } from "discord.js";
+import { Client, GuildMember, Message, User } from "discord.js";
 import * as fs from "fs";
 
 export enum Permission {
@@ -13,6 +13,17 @@ export interface Command {
     "alias"?: string[];
     "permissions": Permission;
     "execute": (msg: Message, args: string[]) => void;
+}
+
+export function checkPermissions(member: GuildMember | User, shouldHave: Permission): boolean {
+    switch (shouldHave) {
+        case Permission.Administrator:
+            if (member instanceof GuildMember && member.hasPermission("ADMINISTRATOR")) return true;
+        case Permission.BotAdmin:
+            if (member.id == "412691653573345290") true;
+        case Permission.Anyone:
+            return true;
+    }
 }
 
 function isCommand(obj: any): obj is Command {
@@ -32,8 +43,13 @@ export async function load(bot: Client) {
     bot.on("message", (msg)=>{
         if (msg.content.startsWith("!")) {
             let args = msg.content.substr(1).split(" ");
-            if (commands[args[0]]) {
-                commands[args[0]].execute(msg, args);
+            let cmd = commands[args[0]];
+            if (cmd) {
+                if (checkPermissions(msg.member ?? msg.author, cmd.permissions)) {
+                    cmd.execute(msg, args);
+                } else {
+                    // TODO: Handle Missing Permissions
+                }
             }
         }
     })
