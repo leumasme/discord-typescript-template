e6ee1e6e1367f00fd43356b19a0a749367113a05
Temm <samumert@gmail.com>
2021-03-23 17:36:51 +0100

    Add Description to Ping Command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,6 +1,7 @@
 import { ChannelTarget, Command, Permission } from "../commandLoader"
 export default ({
     "name": "ping",
+    "description": "Responds to you!",
     "execute": (msg, args) => { msg.channel.send("Hello!") },
     "permissions": Permission.Anyone,
     "alias": ["pong"],
