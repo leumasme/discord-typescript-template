293c19b03cdffda1cc66fff24b256e3cd0319ef8
Temm <samumert@gmail.com>
2021-04-10 18:39:51 +0200

    Add counter command to demo database

---
+++ src/commands/counter.ts
@@ -0,0 +1,46 @@
+import { db } from "..";
+import { ChannelTarget, Command, Permission } from "../commandLoader"
+export default ({
+    "name": "counter",
+    "description": "A counter demonstrating persistant data!",
+    "execute": async (msg, args) => {
+        switch (args[1]) {
+            case "add":
+                await db.member.upsert({
+                    create: {
+                        guildid: msg.guild!.id,
+                        userid: msg.author.id
+                    },
+                    update: {
+                        counter: {
+                            increment: 1
+                        }
+                    },
+                    where: {
+                        guildid_userid: {
+                            guildid: msg.guild!.id,
+                            userid: msg.author.id
+                        }
+                    }
+                });
+                msg.channel.send("Your counter has been increased!")
+                break;
+            case "view":
+                let counter = await db.member.findUnique({
+                    where: {
+                        guildid_userid: {
+                            guildid: msg.guild!.id,
+                            userid: msg.author.id
+                        }
+                    },
+                    select: {
+                        counter: true
+                    }
+                })
+                msg.channel.send("Your counter is currently "+((counter?.counter) ?? "not set"))
+                break;
+        }
+    },
+    "permissions": Permission.Anyone,
+    "channels": ChannelTarget.GuildOnly
+} as Command)
